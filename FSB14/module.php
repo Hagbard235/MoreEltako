38d18cf16e45d3ec492cdfdd776ec6bbba85ab9c
Hagbard235 <github@andreschmidt.de>
2018-05-24 17:24:13 +0200

    Update module.php

--- FSB14/module.php
+++ FSB14/module.php
@@ -30,7 +30,7 @@
 
 			
 			
-		//	$this->SetReceiveDataFilter(".*".hexdec($this->ReadPropertyString("DeviceIDRet")).".*");
+			$this->SetReceiveDataFilter(".*".hexdec($this->ReadPropertyString("DeviceIDRet")).".*");
 		}
 		
 		/*
@@ -50,7 +50,7 @@
 			$data = json_decode($JSONString);
 			$this->SendDebug("EnoceanGatewayData", $JSONString, 0);
 			
-			IPS_LogMessage("FSB14 Device ID (HEX)",dechex($data->{'DeviceID'}));
+			//IPS_LogMessage("FSB14 Device ID (HEX)",dechex($data->{'DeviceID'}));
 			
 			// fahrzeit auswerten
 			$this->Process($data);
